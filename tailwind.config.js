044787984f7b09f8625cb749879b0e81328bf189
xorostar <aswadali.me@gmail.com>
2025-01-24 17:28:02 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+            },
+            colors: {
+                'gray-light': '#596171',
+                'stripe-blue': '#635bff',
+            },
+        },
+    },
+    plugins: [],
+};
